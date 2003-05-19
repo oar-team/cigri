7024d158814869ebfb8ba97141094e013645f030
capitn <capitn@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2003-05-19 16:43:11 +0000

    utilisatiuon de API scheduler
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/trunk@14 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- Scheduler/sched_fifoCigri.pl
+++ Scheduler/sched_fifoCigri.pl
@@ -2,16 +2,30 @@
 
 use strict;
 use iolibCigri;
+use Data::Dumper;
 
 my $base = iolibCigri::connect();
 
-my $done =0;
-my $doneToLaunch=0;
-
 print "[SCHEDULER] Begining of scheduler FIFO\n";
 
-while (iolibCigri::select_sched_FIFO($base) == 0){
-	iolibCigri::pre_schedule($base);
+my %nbFreeNodes = iolibCigri::get_nb_freeNodes($base);
+my %nbRemainedJobs = iolibCigri::get_nb_remained_jobs($base);
+
+foreach my $i (keys(%nbRemainedJobs)){
+	my @propertiesClusterName = iolibCigri::get_MJobs_Properties($base, $i);
+	foreach my $j (@propertiesClusterName){
+		my $number ;
+		if ($nbRemainedJobs{$i} <= $nbFreeNodes{$j}){
+			$number = $nbRemainedJobs{$i};
+		}else{
+			$number = $nbFreeNodes{$j};
+		}
+		if ($number > 0){
+			iolibCigri::add_job_to_launch($base,$i,$j,$number);
+			$nbFreeNodes{$j} -= $number;
+			$nbRemainedJobs{$i} -= $number;
+		}
+	}
 }
 
 iolibCigri::disconnect($base);
