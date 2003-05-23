874b760f7a0c2aa0bbdbdceac4abf5cbff0ba7ca
capitn <capitn@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2003-05-23 16:47:05 +0000

    mise en place d un canal ssh par cluster
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/trunk@24 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- Runner/runnerCigri.pl
+++ Runner/runnerCigri.pl
@@ -21,7 +21,7 @@ BEGIN {
 	unshift(@INC, $relativePath."Iolib");
 }
 use iolibCigri;
-use Net::SSH;
+use SSHcmd;
 
 # List of pbsnodes commands
 my %qsubCommand = ( 'PBS' => 'qsub',
@@ -29,51 +29,51 @@ my %qsubCommand = ( 'PBS' => 'qsub',
 
 my $base = iolibCigri::connect() ;
 
-sub forkSSH ($$$$){
-	my $cluster = shift;
-	my $jobId = shift;
-	my $batch = shift;
-	my $cmdref = shift;
-	my @cmd = @$cmdref;
-	my $pid = 0;
-
-	$pid = fork();
-	if($pid eq "undef"){
-		$pid = -1;
-	}
-	if($pid != 0){
-		#father
-		$SIG{CHLD} = "IGNORE";
-		return $pid;
-	}else{
-		#child
-		select(STDOUT);
-		$| = 1;
-		print("[RUNNER] I launch the job $jobId on the cluster $cluster\n");
-		Net::SSH::sshopen3($cluster, *WRITER, *READER, *ERROR, @cmd) || die "[RUNNER] ssh ERROR : $!";
-		close(WRITER);
-		if (defined(<ERROR>)){
-			while(<ERROR>){
-				print("[RUNNER_STDERR]$_");
-			}
-			close(ERROR);
-			iolibCigri::set_job_state($base,$jobId,"Error");
-		}elsif (defined(<READER>)){
-			while (<READER>){
-				if (($batch eq "OAR") and ($_ =~ /\s*IdJob\s=\s(\d+)/)){
-					iolibCigri::set_job_batch_id($base,$jobId,$1);
-				}
-				print("[RUNNER_STDOUT] $_");
-			}
-			close(READER);
-			iolibCigri::set_job_state($base,$jobId,"Running");
-		}else{
-			print("[RUNNER] There is a mistake, the job $jobId state is unchanged\n");
-		}
-		exit 0;
-	}
-	return $pid;
-}
+#sub forkSSH ($$$$){
+#	my $cluster = shift;
+#	my $jobId = shift;
+#	my $batch = shift;
+#	my $cmdref = shift;
+#	my @cmd = @$cmdref;
+#	my $pid = 0;
+#
+#	$pid = fork();
+##	if($pid eq "undef"){
+##		$pid = -1;
+#	}
+#	if($pid != 0){
+#		#father
+#		$SIG{CHLD} = "IGNORE";
+#		return $pid;
+#	}else{
+#		#child
+#		select(STDOUT);
+#		$| = 1;
+#		print("[RUNNER] I launch the job $jobId on the cluster $cluster\n");
+#		Net::SSH::sshopen3($cluster, *WRITER, *READER, *ERROR, @cmd) || die "[RUNNER] ssh ERROR : $!";
+#		close(WRITER);
+#		if (defined(<ERROR>)){
+#			while(<ERROR>){
+#				print("[RUNNER_STDERR]$_");
+#			}
+#			close(ERROR);
+#			iolibCigri::set_job_state($base,$jobId,"Error");
+#		}elsif (defined(<READER>)){
+#			while (<READER>){
+#				if (($batch eq "OAR") and ($_ =~ /\s*IdJob\s=\s(\d+)/)){
+#					iolibCigri::set_job_batch_id($base,$jobId,$1);
+#				}
+#				print("[RUNNER_STDOUT] $_");
+#			}
+#			close(READER);
+#			iolibCigri::set_job_state($base,$jobId,"Running");
+#		}else{
+#			print("[RUNNER] There is a mistake, the job $jobId state is unchanged\n");
+#		}
+#		exit 0;
+#	}
+#	return $pid;
+#}
 
 # treate the scheduler output in the jobsToSubmit table
 if (iolibCigri::create_toLaunch_jobs($base) == 1){
@@ -102,10 +102,7 @@ foreach my $i (@jobList){
 
 	#iolibCigri::set_job_state($base,$jobId,"Launching");
 
-	my @cmdSSH = ();
-
-	if ($$i{batch} eq "OAR"){
-		@cmdSSH = (	"echo \\#\\!/bin/sh > ~/$tmpRemoteFile;",
+	my @cmdSSH = (	"echo \\#\\!/bin/sh > ~/$tmpRemoteFile;",
 					"echo \"echo \\\"BEGIN_DATE=\\\"\\`date +\%Y-\%m-\%d\\ \%H:\%M:\%S\\` >> ~/$resultFile\" >> ~/$tmpRemoteFile;",
 					"echo $$i{cmd} $$i{param} >> ~/$tmpRemoteFile;",
 					"echo CODE=\\\$? >> ~/$tmpRemoteFile;",
@@ -118,23 +115,31 @@ foreach my $i (@jobList){
 					"sudo -u $$i{user} /bin/cp ~/$tmpRemoteFile . ;",
 					"rm ~/$tmpRemoteFile ;",
 					"sudo -u $$i{user} $qsubCommand{$$i{batch}} -l nodes=1 `pwd`/$tmpRemoteFile;"
-		);
+	);
+
+	my $cmdString = join(" ", @cmdSSH);
+	my %cmdResult = SSHcmd::submitCmd($$i{clusterName},$cmdString);
+print(Dumper(%cmdResult));
+	if ($cmdResult{STDERR} ne ""){
+		print("[RUNNER_STDERR] $cmdResult{STDERR}");
+		iolibCigri::set_job_state($base,$jobId,"Error");
+	}elsif ($cmdResult{STDOUT} ne ""){
+		my @strTmp = split(/\n/, $cmdResult{STDOUT});
+		my $configured = 0;
+		foreach my $k (@strTmp){
+			if ($k =~ /\s*IdJob\s=\s(\d+)/){
+				iolibCigri::set_job_batch_id($base,$jobId,$1);
+				$configured = 1;
+			}
+			print("[RUNNER_STDOUT] $k\n");
+		}
+		if ($configured == 1){
+			iolibCigri::set_job_state($base,$jobId,"Running");
+		}else{
+			print("[RUNNER] There is a mistake, the job $jobId state is unchanged, bad remote batch id\n");
+		}
 	}else{
-		@cmdSSH = (	"echo \\#\\!/bin/sh > ~/$tmpRemoteFile;",
-					"echo $$i{cmd} $$i{param} >> ~/$tmpRemoteFile;",
-					"echo rm ~$$i{user}/$tmpRemoteFile >> ~/$tmpRemoteFile;",
-					"chmod +x ~/$tmpRemoteFile ;",
-					"cd ~$$i{user} ;",
-					"sudo -u $$i{user} /bin/cp ~/$tmpRemoteFile . ;",
-					"rm ~/$tmpRemoteFile ;",
-					"$qsubCommand{$$i{batch}} -l nodes=$$i{node} $tmpRemoteFile;"
-		);
-	}
-	if(defined(@cmdSSH)){
-		#print("[RUNNER]".Dumper(@cmdSSH)."\n");
-		forkSSH($$i{clusterName},$jobId,$$i{batch},\@cmdSSH);
+		print("[RUNNER] There is a mistake, the job $jobId state is unchanged\n");
 	}
 }
-#wait;
-
 iolibCigri::disconnect($base);
