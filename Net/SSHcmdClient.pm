41cfa84723c6ea62c39d595d913daf8e972d84b5
capitn <capitn@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2003-07-16 14:02:21 +0000

    introduction de la variable SSH_SERVER_PORT
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/trunk@71 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- Net/SSHcmdClient.pm
+++ Net/SSHcmdClient.pm
@@ -4,10 +4,34 @@ use strict;
 use warnings;
 use IO::Socket::INET;
 use Data::Dumper;
+
+BEGIN {
+	my ($scriptPathTmp) = $0 =~ m!(.*/*)!s;
+	my ($scriptPath) = readlink($scriptPathTmp);
+	if (!defined($scriptPath)){
+		$scriptPath = $scriptPathTmp;
+	}
+	# Relative path of the package
+	my @relativePathTemp = split(/\//, $scriptPath);
+	my $relativePath = "";
+	for (my $i = 0; $i < $#relativePathTemp; $i++){
+		$relativePath = $relativePath.$relativePathTemp[$i]."/";
+	}
+	$relativePath = $relativePath."../";
+	# configure the path to reach the lib directory
+	unshift(@INC, $relativePath."lib");
+	unshift(@INC, $relativePath."ConfLib");
+	unshift(@INC, $relativePath."Net");
+}
+use ConfLibCigri qw(init_conf dump_conf get_conf is_conf);
+
 require Exporter;
 our (@ISA,@EXPORT,@EXPORT_OK);
 @ISA = qw(Exporter);
 @EXPORT_OK = qw(submitCmd);
+
+ConfLibCigri::init_conf();
+my $sshServerPort = ConfLibCigri::get_conf("SSH_SERVER_PORT") if ConfLibCigri::is_conf("SSH_SERVER_PORT") or die("Can't get value of the SSH_SERVER_PORT tag in cigri.conf\n");
 
 # submit a command to the given cluster
 # arg1 --> clusterName
@@ -16,7 +40,7 @@ sub submitCmd($$){
 	my $clusterName = shift;
 	my $command = shift;
 	my $socket = IO::Socket::INET->new(	PeerAddr => "127.0.0.1",
-									PeerPort => 6694,
+									PeerPort => $sshServerPort,
 									Proto => "tcp",
 									Type  => SOCK_STREAM)
 					or die "Couldn't connect to SSH daemon $@ \n";
--- Net/SSHcmdServer.pl
+++ Net/SSHcmdServer.pl
@@ -1,14 +1,36 @@
 #!/usr/bin/perl
 
+use strict;
 use warnings;
 use Data::Dumper;
 use IO::Socket::INET;
+BEGIN {
+	my ($scriptPathTmp) = $0 =~ m!(.*/*)!s;
+	my ($scriptPath) = readlink($scriptPathTmp);
+	if (!defined($scriptPath)){
+		$scriptPath = $scriptPathTmp;
+	}
+	# Relative path of the package
+	my @relativePathTemp = split(/\//, $scriptPath);
+	my $relativePath = "";
+	for (my $i = 0; $i < $#relativePathTemp; $i++){
+		$relativePath = $relativePath.$relativePathTemp[$i]."/";
+	}
+	$relativePath = $relativePath."../";
+	# configure the path to reach the lib directory
+	unshift(@INC, $relativePath."lib");
+	unshift(@INC, $relativePath."ConfLib");
+	unshift(@INC, $relativePath."Net");
+}
+use ConfLibCigri qw(init_conf dump_conf get_conf is_conf);
 use SSHcmd;
 
 my $markerTag = "<SSHserverTag>";
 
-# Server part
-my $server = IO::Socket::INET->new(	LocalPort=> 6694,
+ConfLibCigri::init_conf();
+my $sshServerPort = ConfLibCigri::get_conf("SSH_SERVER_PORT") if ConfLibCigri::is_conf("SSH_SERVER_PORT") or die("Can't get value of the SSH_SERVER_PORT tag in cigri.conf\n");
+
+my $server = IO::Socket::INET->new(	LocalPort=> $sshServerPort,
 									Type => SOCK_STREAM,
 									Reuse => 1,
 									Listen => 10)
