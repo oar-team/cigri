2e11d5981456a2e5327693f0ab74b5f129273687
capitn <capitn@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2003-09-15 16:12:12 +0000

    *** empty log message ***
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/trunk@110 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- Almighty/AlmightyCigri.pl
+++ Almighty/AlmightyCigri.pl
@@ -3,23 +3,23 @@ use strict;
 use Data::Dumper;
 use IO::Socket::INET;
 BEGIN {
-	my ($scriptPathTmp) = $0 =~ m!(.*/*)!s;
-	my ($scriptPath) = readlink($scriptPathTmp);
-	if (!defined($scriptPath)){
-		$scriptPath = $scriptPathTmp;
-	}
-	# Relative path of the package
-	my @relativePathTemp = split(/\//, $scriptPath);
-	my $relativePath = "";
-	for (my $i = 0; $i < $#relativePathTemp; $i++){
-		$relativePath = $relativePath.$relativePathTemp[$i]."/";
-	}
-	$relativePath = $relativePath."../";
-	# configure the path to reach the lib directory
-	unshift(@INC, $relativePath."lib");
-	unshift(@INC, $relativePath."ConfLib");
-	unshift(@INC, $relativePath."Iolib");
-	unshift(@INC, $relativePath."Colombo");
+    my ($scriptPathTmp) = $0 =~ m!(.*/*)!s;
+    my ($scriptPath) = readlink($scriptPathTmp);
+    if (!defined($scriptPath)){
+        $scriptPath = $scriptPathTmp;
+    }
+    # Relative path of the package
+    my @relativePathTemp = split(/\//, $scriptPath);
+    my $relativePath = "";
+    for (my $i = 0; $i < $#relativePathTemp; $i++){
+        $relativePath = $relativePath.$relativePathTemp[$i]."/";
+    }
+    $relativePath = $relativePath."../";
+    # configure the path to reach the lib directory
+    unshift(@INC, $relativePath."lib");
+    unshift(@INC, $relativePath."ConfLib");
+    unshift(@INC, $relativePath."Iolib");
+    unshift(@INC, $relativePath."Colombo");
 }
 use ConfLibCigri qw(init_conf dump_conf get_conf is_conf);
 use iolibCigri;
@@ -32,9 +32,9 @@ my $timeout = 3;
 
 my $path;
 if (is_conf("installPath")){
-	$path = get_conf("installPath")."/bin/";
+    $path = get_conf("installPath")."/bin/";
 }else{
-	die("You must have a cigri.conf script with a valid installPath tag\n");
+    die("You must have a cigri.conf script with a valid installPath tag\n");
 }
 
 #set paths of executables
@@ -48,64 +48,64 @@ my $base = iolibCigri::connect();
 # arg1 --> command name
 sub launch_command($)
 {
-	my $command = shift;
-	print "Launching command : [$command]\n";
-	system $command;
-	my $exit_value  = $? >> 8;
-	my $signal_num  = $? & 127;
-	my $dumped_core = $? & 128;
-	print "$command terminated :\n";
-	print "Exit value : $exit_value\n";
-	print "Signal num : $signal_num\n";
-	print "Core dumped : $dumped_core\n";
-	die "Something wrong occured (signal or core dumped) !!!\n"
-		if $signal_num || $dumped_core;
-	return $exit_value;
+    my $command = shift;
+    print "Launching command : [$command]\n";
+    system $command;
+    my $exit_value  = $? >> 8;
+    my $signal_num  = $? & 127;
+    my $dumped_core = $? & 128;
+    print "$command terminated :\n";
+    print "Exit value : $exit_value\n";
+    print "Signal num : $signal_num\n";
+    print "Core dumped : $dumped_core\n";
+    die "Something wrong occured (signal or core dumped) !!!\n"
+        if $signal_num || $dumped_core;
+    return $exit_value;
 }
 
 # launch a scheduler or blacklist it
 sub scheduler(){
-	iolibCigri::update_current_scheduler($base);
-	my $sched = iolibCigri::get_current_scheduler($base);
-	#return launch_command($scheduler_command);
-	if (defined($$sched{schedulerFile})){
-		if ( -x $path.$$sched{schedulerFile} ){
-			my $exitValue = launch_command($path.$$sched{schedulerFile});
+    iolibCigri::update_current_scheduler($base);
+    my $sched = iolibCigri::get_current_scheduler($base);
+    #return launch_command($scheduler_command);
+    if (defined($$sched{schedulerFile})){
+        if ( -x $path.$$sched{schedulerFile} ){
+            my $exitValue = launch_command($path.$$sched{schedulerFile});
             if ($exitValue != 0){
                 colomboCigri::add_new_scheduler_event($base,$$sched{schedulerId},"EXIT_VALUE","bad exit value $exitValue for $path.$$sched{schedulerFile}");
             }
             return $exitValue;
-		}else{
-			print("Bad scheduler file\n");
-			#iolibCigri::insert_new_schedulerError($base,"FILE","Can t find the file $$sched{schedulerFile}");
-			colomboCigri::add_new_scheduler_event($base,$$sched{schedulerId},"ALMIGHTY_FILE","Can t find the file $path.$$sched{schedulerFile}");
-		}
-	}else{
-		print("NO SCHEDULER TO LAUNCH :-(\n");
-	}
+        }else{
+            print("Bad scheduler file\n");
+            #iolibCigri::insert_new_schedulerError($base,"FILE","Can t find the file $$sched{schedulerFile}");
+            colomboCigri::add_new_scheduler_event($base,$$sched{schedulerId},"ALMIGHTY_FILE","Can t find the file $path.$$sched{schedulerFile}");
+        }
+    }else{
+        print("NO SCHEDULER TO LAUNCH :-(\n");
+    }
 }
 
 # launch the runner command
 sub runner(){
-	return launch_command($runner_command);
+    return launch_command($runner_command);
 }
 
 # launch updator command
 sub updator(){
-	return launch_command($updator_command);
+    return launch_command($updator_command);
 }
 
 # launch nikita command
 sub nikita(){
-	return launch_command($nikita_command);
+    return launch_command($nikita_command);
 }
 
 # core of the AlmightyCigri
 while (1){
-	nikita();
-	updator();
-	scheduler();
-	runner();
-	print("I make a pause of $timeout seconds :-)\n");
-	sleep($timeout);
+    nikita();
+    updator();
+    scheduler();
+    runner();
+    print("I make a pause of $timeout seconds :-)\n");
+    sleep($timeout);
 }
