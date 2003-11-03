262bb35d7908c9afd1360c5771dc1730d248256f
capitn <capitn@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2003-11-03 16:35:22 +0000

    *** empty log message ***
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/trunk@146 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- Net/SSHcmdClient.pm
+++ Net/SSHcmdClient.pm
@@ -26,13 +26,14 @@ BEGIN {
 	unshift(@INC, $relativePath."Net");
 }
 use ConfLibCigri qw(init_conf dump_conf get_conf is_conf);
-
+use NetCommon;
 
 require Exporter;
 our (@ISA,@EXPORT,@EXPORT_OK);
 @ISA = qw(Exporter);
 @EXPORT_OK = qw(submitCmd);
 
+my $sshErrorPrefix = NetCommon::getSshErrorPrefix();
 ConfLibCigri::init_conf();
 my $sshServerPort = ConfLibCigri::get_conf("SSH_SERVER_PORT") if ConfLibCigri::is_conf("SSH_SERVER_PORT") or die("Can't get value of the SSH_SERVER_PORT tag in cigri.conf\n");
 
@@ -43,39 +44,43 @@ my $sshServerPort = ConfLibCigri::get_conf("SSH_SERVER_PORT") if ConfLibCigri::i
 sub submitCmd($$){
 	my $clusterName = shift;
 	my $command = shift;
-	my $socket = IO::Socket::INET->new(	PeerAddr => "127.0.0.1",
-									PeerPort => $sshServerPort,
-									Proto => "tcp",
-									Type  => SOCK_STREAM)
-					or die "Couldn't connect to SSH daemon $@ \n";
 
-	print $socket "<submitCmd> $clusterName $command <\\submitCmd>\n";
+    my %cmdResult = ("STDOUT" => "",
+                     "STDERR" => ""
+                    );
 
-	my $result = 0;
-	my %cmdResult = ( 	"STDOUT" => "",
-						"STDERR" => ""
-					);
+    my $socket = IO::Socket::INET->new(	PeerAddr => "127.0.0.1",
+                                        PeerPort => $sshServerPort,
+                                        Proto => "tcp",
+                                        Type  => SOCK_STREAM);
+#					or die "Couldn't connect to SSH daemon $@ \n";
+    if (defined($socket)){
+        print $socket "<submitCmd> $clusterName $command <\\submitCmd>\n";
 
-	while (<$socket>){
-		chomp;
-		if (substr($_,0,2) eq "<\\"){
-			$result = 0;
-		}
+        my $result = 0;
 
-		if ($result == 1){
-			$cmdResult{STDOUT} .= $_."\n" ;
-		}elsif ($result == 2){
-			$cmdResult{STDERR} .= $_."\n" ;
-		}
+        while (<$socket>){
+            chomp;
+            if (substr($_,0,2) eq "<\\"){
+                $result = 0;
+            }
 
-		if ( "$_" eq "<SSHserverTagSTDOUT>"){
-			$result = 1;
-		}elsif ("$_" eq "<SSHserverTagSTDERR>"){
-			$result = 2;
-		}
-	}
+            if ($result == 1){
+                $cmdResult{STDOUT} .= $_."\n" ;
+            }elsif ($result == 2){
+                $cmdResult{STDERR} .= $_."\n" ;
+            }
 
-#	print("\n".Dumper(%cmdResult));
+            if ( "$_" eq "<SSHserverTagSTDOUT>"){
+                $result = 1;
+            }elsif ("$_" eq "<SSHserverTagSTDERR>"){
+                $result = 2;
+            }
+        }
+    }else{
+        $cmdResult{STDERR} = "$sshErrorPrefix Can t connect to the local SSH server\n";
+    }
+    #	print("\n".Dumper(%cmdResult));
 	return %cmdResult;
 }
 
