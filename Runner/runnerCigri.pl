2a2f46b5a16eafd0dbba56f7173f090ae1fda691
capitn <capitn@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2003-12-07 21:37:01 +0000

    deroulement cluster/cluster
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/trunk@157 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- Runner/runnerCigri.pl
+++ Runner/runnerCigri.pl
@@ -1,4 +1,4 @@
-#! /usr/bin/perl
+#!/usr/bin/perl
 
 # This program launch the jobs on remote clusters
 
@@ -30,86 +30,67 @@ use SSHcmdClient;
 use NetCommon;
 use jobSubmit;
 
-# List of pbsnodes commands
-my %qsubCommand = ( 'PBS' => 'qsub',
-					'OAR1' => 'qsub.pl',
-					'OAR' => 'oarsub',
-                    'OAR_mysql' => 'oarsub');
-
 my $base = iolibCigri::connect() ;
 
-# treate the scheduler output in the jobsToSubmit table
-if (iolibCigri::create_toLaunch_jobs($base) == 1){
-	warn("[Runner] Error when i create_toLaunch_jobs\n");
-	exit 1;
-}
+# Get active cluster names
+my %clusterNames = iolibCigri::get_cluster_names_batch($base);
+my %job;
+foreach my $j (keys(%clusterNames)){
+    print("[RUNNER] check for cluster $j\n");
+    while (iolibCigri::get_cluster_job_toLaunch($base,$j,\%job) == 0){
+        print("[Runner] Launch the job $job{id} on the node $job{nodeId}\n");
+        print(Dumper(%job));
 
-# list of jobs to launch
-my @jobList = iolibCigri::get_launching_job($base);
+        my $jobId = $job{id};
+        my $tmpRemoteFile = "cigri.tmp.$jobId";
+        my $resultFile = "~/".iolibCigri::get_cigri_remote_file_name($jobId);
 
-print(Dumper(@jobList));
+        print("[RUNNER] The job $jobId is in treatment...\n");
 
-my $jobId;
-my $jobtype;
-my $jobinfo;
+        # command to launch on the frontal of the cluster
+        my @cmdSSH = (	"echo \\#\\!/bin/sh > ~/$tmpRemoteFile;",
+					    "echo \"echo \\\"BEGIN_DATE=\\\"\\`date +\%Y-\%m-\%d\\ \%H:\%M:\%S\\` >> $resultFile\" >> ~/$tmpRemoteFile;",
+                        "echo $job{cmd} $job{param} >> ~/$tmpRemoteFile;",
+                        "echo CODE=\\\$? >> ~/$tmpRemoteFile;",
+                        "echo \"echo \\\"END_DATE=\\\"\\`date +\%Y-\%m-\%d\\ \%H:\%M:\%S\\` >> $resultFile\" >> ~/$tmpRemoteFile;",
+                        "echo \"echo \\\"RET_CODE=\\\$CODE\\\" >> $resultFile\" >> ~/$tmpRemoteFile;",
+                        "echo \"echo \\\"FINISH=1\\\" >> $resultFile\" >> ~/$tmpRemoteFile;",
+                        "echo rm ~$job{user}/$tmpRemoteFile >> ~/$tmpRemoteFile;",
+                        "chmod +x ~/$tmpRemoteFile ;",
+                        "cd ~$job{user} ;",
+                        "sudo -u $job{user} /bin/cp ~/$tmpRemoteFile . ;",
+                        "rm ~/$tmpRemoteFile ;"
+                        #"sudo -u $job{user} $qsubCommand{$job{batch}} -l nodes=1 -q besteffort `pwd`/$tmpRemoteFile;"
+                   );
 
-my $tmpRemoteFile ;
-my $resultFile ;
-
-select(STDOUT);
-$| = 1;
-
-foreach my $i (@jobList){
-if (colomboCigri::is_cluster_active($base,"$i",0) == 0){
-	$jobId = $$i{id};
-	$tmpRemoteFile = "cigri.tmp.$jobId";
-	$resultFile = "~/".iolibCigri::get_cigri_remote_file_name($jobId);
-
-	print("[RUNNER] The job $jobId is in treatment...\n");
-
-	# command to launch on the frontal of the cluster
-	my @cmdSSH = (	"echo \\#\\!/bin/sh > ~/$tmpRemoteFile;",
-					"echo \"echo \\\"BEGIN_DATE=\\\"\\`date +\%Y-\%m-\%d\\ \%H:\%M:\%S\\` >> $resultFile\" >> ~/$tmpRemoteFile;",
-					"echo $$i{cmd} $$i{param} >> ~/$tmpRemoteFile;",
-					"echo CODE=\\\$? >> ~/$tmpRemoteFile;",
-					"echo \"echo \\\"END_DATE=\\\"\\`date +\%Y-\%m-\%d\\ \%H:\%M:\%S\\` >> $resultFile\" >> ~/$tmpRemoteFile;",
-					"echo \"echo \\\"RET_CODE=\\\$CODE\\\" >> $resultFile\" >> ~/$tmpRemoteFile;",
-					"echo \"echo \\\"FINISH=1\\\" >> $resultFile\" >> ~/$tmpRemoteFile;",
-					"echo rm ~$$i{user}/$tmpRemoteFile >> ~/$tmpRemoteFile;",
-					"chmod +x ~/$tmpRemoteFile ;",
-					"cd ~$$i{user} ;",
-					"sudo -u $$i{user} /bin/cp ~/$tmpRemoteFile . ;",
-					"rm ~/$tmpRemoteFile ;"
-#					"sudo -u $$i{user} $qsubCommand{$$i{batch}} -l nodes=1 -q besteffort `pwd`/$tmpRemoteFile;"
-                );
-
-	my $cmdString = join(" ", @cmdSSH);
-	my %cmdResult = SSHcmdClient::submitCmd($$i{clusterName},$cmdString);
-	if ($cmdResult{STDERR} ne ""){
-		print("[RUNNER_STDERR] $cmdResult{STDERR}");
-		# test if this is a ssh error
-        if (NetCommon::checkSshError($base,$$i{clusterName},$cmdResult{STDERR}) != 1){
-            iolibCigri::set_job_state($base,$jobId,"Event");
-            # treate the SSH error
-            colomboCigri::add_new_job_event($base,$jobId,"RUNNER_SUBMIT",$cmdResult{STDERR});
-        }
-        exit(66);
-	}else{
-		my $retCode = jobSubmit::jobSubmit($$i{clusterName},$$i{user},$tmpRemoteFile);
-        if ($retCode < 0){
-            if ($retCode == -2){
-                print("[RUNNER] There is a mistake, the job $jobId state = ERROR, bad remote batch id\n");
-                iolibCigri::set_job_state($base, $jobId, "Event");
-                colomboCigri::add_new_job_event($base,$jobId,"RUNNER_JOBID_PARSE","There is a mistake, the job $jobId state = ERROR, bad remote batch id");
+        my $cmdString = join(" ", @cmdSSH);
+        my %cmdResult = SSHcmdClient::submitCmd($job{clusterName},$cmdString);
+        if ($cmdResult{STDERR} ne ""){
+            print("[RUNNER_STDERR] $cmdResult{STDERR}");
+            # test if this is a ssh error
+            if (NetCommon::checkSshError($base,$job{clusterName},$cmdResult{STDERR}) != 1){
+                iolibCigri::set_job_state($base,$jobId,"Event");
+                # treate the SSH error
+                colomboCigri::add_new_job_event($base,$jobId,"RUNNER_SUBMIT",$cmdResult{STDERR});
             }
             exit(66);
         }else{
-            iolibCigri::set_job_batch_id($base,$jobId,$retCode);
-			iolibCigri::set_job_state($base,$jobId,"Running");
+            my $retCode = jobSubmit::jobSubmit($job{clusterName},$job{user},$tmpRemoteFile);
+            if ($retCode < 0){
+                if ($retCode == -2){
+                    print("[RUNNER] There is a mistake, the job $jobId state = ERROR, bad remote batch id\n");
+                    iolibCigri::set_job_state($base, $jobId, "Event");
+                    colomboCigri::add_new_job_event($base,$jobId,"RUNNER_JOBID_PARSE","There is a mistake, the job $jobId state = ERROR, bad remote batch id");
+                }
+                exit(66);
+            }else{
+                iolibCigri::set_job_batch_id($base,$jobId,$retCode);
+                iolibCigri::set_job_state($base,$jobId,"Running");
+            }
         }
     }
-}else{
-    print("[RUNNER] cluster blacklisted $$i{clusterName}\n");
 }
-}
+
 iolibCigri::disconnect($base);
+
+exit(0)
