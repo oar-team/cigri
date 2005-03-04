68ff1a962af60632d35789f725f220989aff0a2b
capitn <capitn@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2005-03-04 10:10:24 +0000

    verif du user
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/trunk@240 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- Qfunctions/griddel.pl
+++ Qfunctions/griddel.pl
@@ -53,24 +53,44 @@ my $jobId = undef;
 
 my $base = iolibCigri::connect();
 
+my $user = getpwuid($<);
+my $exit_code = 0;
 
 if (defined($opts{"m"})){
     $MJobId = $opts{"m"};
-    colomboCigri::add_new_mjob_event($base,$MJobId,"FRAG","user frag event");
-    print("Delete the MJob $MJobId\n");
+    my $MJobUser = iolibCigri::get_MJob_user($base,$MJobId);
+    if ($MJobUser eq $user){
+        colomboCigri::add_new_mjob_event($base,$MJobId,"FRAG","user frag event");
+        print("Delete the MJob $MJobId\n");
+    }else{
+        print("/!\\ You are not the right user\n");
+        $exit_code = 1;
+    }
 }elsif(defined($opts{"j"})){
     $jobId = $opts{"j"};
-    iolibCigri::set_job_state($base, $jobId, "Event");
-    colomboCigri::add_new_job_event($base,$jobId,"FRAG","user frag event");
-    print("Delete the job $jobId\n");
+    my $jobUser = iolibCigri::get_job_user($base,$jobId);
+    if ($jobUser eq $user){
+        iolibCigri::set_job_state($base, $jobId, "Event");
+        colomboCigri::add_new_job_event($base,$jobId,"FRAG","user frag event");
+        print("Delete the job $jobId\n");
+    }else{
+        print("/!\\ You are not the right user\n");
+        $exit_code = 1;
+    }
 }elsif(defined($opts{"r"})){
     $jobId = $opts{"r"};
-    colomboCigri::resubmit_job($base,$jobId);
-    print("Parameters of job $jobId is resubmitted\n");
+    my $jobUser = iolibCigri::get_job_user($base,$jobId);
+    if ($jobUser eq $user){
+        colomboCigri::resubmit_job($base,$jobId);
+        print("Parameters of job $jobId is resubmitted\n");
+    }else{
+        print("/!\\ You are not the right user\n");
+        $exit_code = 1;
+    }
 }else{
     usage();
 }
 
 iolibCigri::disconnect($base);
 
-exit 0;
+exit $exit_code;
