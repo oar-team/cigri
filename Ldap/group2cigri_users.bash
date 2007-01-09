e3a67a508623eccfeaa06b5e114e5f5f05865a81
bzizou <bzizou@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2007-01-09 10:22:52 +0000

    Initiated devel of a script to automaticaly insert users into CiGri from a Unix group
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/trunk@269 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

---
+++ Ldap/group2cigri_users.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Intented to be used on a CIGRI host connected to an LDAP server
+# on which all the clusters are also connected.
+# A special group defines the cigri users that are to be automaticaly
+# included into the cigri user's table
+
+CIGRI_GROUP=oar
+CIGRI_CONFIGFILE=/etc/cigri.conf
+
+
+# Load the cigri MySQL variables
+CONF=`mktemp`
+sed "s/ = /=/" /etc/cigri.conf > $CONF
+. $CONF
+rm -f $CONF
+
+# Get the cluster list
+OPTS="-h $DATABASE_HOST -u $DATABASE_USER_NAME -p$DATABASE_USER_PASSWORD -D $DATABASE_NAME --skip-column-names" 
+echo $OPTS
+mysql $OPTS -e "select * from clusters"
+
+
+# Get the users of the cigri group
+USERS=`getent group $CIGRI_GROUP |cut -f 4 -d:|sed "s/,/ /g"`
+for user in $USERS
+do
+  echo $user
+done
+
+# To be continued...
