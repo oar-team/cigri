b7275dda9df54eb48870ceb72bd2b54d67703e95
auguste <auguste@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2007-02-28 21:17:08 +0000

    fix std_deviation and duration
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/trunk@281 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- Spritz/gridforecast.rb
+++ Spritz/gridforecast.rb
@@ -126,7 +126,8 @@ def forecast(dbh,mjob)
 			end		
 	end
 
-	std_dev = 0 
+	std_dev = terminated.first * terminated.first 
+
   nb = terminated.length
 	# Sum some numbers
 	total = terminated.inject {|sum, n| std_dev += n * n; sum + n }
@@ -154,7 +155,8 @@ def forecast(dbh,mjob)
 		puts "Mjob: #{mjob_id} Terminated jobs: nb: #{nb}  time: #{total} mean: #{mean} std dev: #{std_dev}" if $verbose
 		puts "Mjob: #{mjob_id} Remains 	 jobs: nb: #{nb_waiting} forcasted time: #{forcasted} sec or 	#{stodhm(forcasted)}"  if $verbose
 		res['status'] = "ok"
-		res['end_time'] =  sprintf("%.0f",Time.now.to_f + forcasted) 
+		res['end_time'] =  sprintf("%.0f",Time.now.to_f + forcasted)
+		res['duration'] = forcasted
 		res['data'] =  {'average' => sprintf("%.2f",mean) , 'standard_deviation' => sprintf("%.2f",std_dev) } 
 
 	end
