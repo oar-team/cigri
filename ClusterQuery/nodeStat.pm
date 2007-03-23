e1ee6f951d0cf3985308374f1a7e165639fbc8b6
bzizou <bzizou@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2007-03-23 11:32:39 +0000

    Bugfix: rewrited oarnodes2() for OAR2 scheduler: free resources were very badly calculated because of the use of the --backward option. Now, we're using oar2 with Dumper behavior (oarnodes -D) for a very precise update of the weight of the nodes.
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/trunk@296 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- ClusterQuery/nodeStat.pm
+++ ClusterQuery/nodeStat.pm
@@ -133,65 +133,53 @@ sub oarnodes($$){
 sub oarnodes2($$){
     my $dbh = shift;
     my $cluster = shift;
-
     print("$cluster --> OAR2\n");
     my %nodeState;
-
+    my $cmd="oarnodes -D";
     my %cmdResult = SSHcmdClient::submitCmd($cluster,"oarnodes --backward");
-    my $pbsnodesStr = $cmdResult{STDOUT};
-    if ($cmdResult{STDERR} eq ""){
-        chomp($pbsnodesStr);
-        my @nodesStrs = split(/^\s*\n/m,$pbsnodesStr);
-        foreach my $nodeStr (@nodesStrs){
-            my @lines = split(/\n/, $nodeStr);
-            my $name = shift(@lines);
-            $name =~ s/\s//g;
-            my $currentWeight;
-            my $maxWeight;
-            my $besteffort;
-            my $state;
-            my $lineTmp;
-            my $key;
-            # parse pbsnodes command
-            while ((! defined($currentWeight) || (! defined($maxWeight)) || (! defined($besteffort))) && ($#lines >= 0)){
-                $lineTmp = shift(@lines);
-                if ($lineTmp =~ /weight =/){
-                    ($key, $currentWeight) = split("=", $lineTmp);
-                    # I drop spaces
-                    $currentWeight =~ s/\s//g;
-                }elsif ($lineTmp =~ /pcpus =/){
-                    ($key, $maxWeight) = split("=", $lineTmp);
-                    # I drop spaces
-                    $maxWeight =~ s/\s//g;
-                }elsif ($lineTmp =~ /properties =/){
-                    $lineTmp =~ /^.+besteffort=(YES|NO).*$/;
-                    $besteffort = $1;
-                }elsif ($lineTmp =~ /state =/){
-                    ($key, $state) = split("=", $lineTmp);
-                    # I drop spaces
-                    $state =~ s/\s//g;
-                }
-
-            }
-            if (defined($name) && defined($maxWeight) && defined($currentWeight) && defined($besteffort) && defined($state)){
-                if (($besteffort eq "YES") && (($state eq "job") || ($state eq "free"))){
-                    # Databse update
-                    iolibCigri::set_cluster_node_free_weight($dbh, $cluster, $name, $maxWeight-$currentWeight);
-                }
-            }else{
-                print("[UPDATOR] There is an error in the oarnodes command parse, node=$name;state=$state\n");
-                colomboCigri::add_new_cluster_event($dbh,$cluster,0,"UPDATOR_PBSNODES_PARSE","There is an error in the oarnodes command parse, node=$name;state=$state");
-                return(-1);
-            }
+    my %cmdDump = SSHcmdClient::submitCmd($cluster,$cmd);
+    my $oarnodesStr = $cmdDump{STDOUT};
+    if ($cmdDump{STDERR} eq ""){
+      my $oarnodes=eval($oarnodesStr);
+      if (defined %{$oarnodes}) {
+        foreach my $node (keys(%{$oarnodes})) {
+           my $jobs=0;
+           my $maxWeight=0;
+           foreach my $resource (keys(%{$oarnodes->{$node}})) {
+             if ("$oarnodes->{$node}->{$resource}->{network_address}" ne "") {
+               $maxWeight++ if ($oarnodes->{$node}->{$resource}->{state} eq "Alive"
+                                 && $oarnodes->{$node}->{$resource}->{properties}->{besteffort} eq "YES");
+                 foreach my $line (keys(%{$oarnodes->{$node}->{$resource}})) {
+                     if ($line eq "jobs") { $jobs++; }
+                 }
+             }else{
+               print("[UPDATOR_ERROR] There is an error in the oarnodes command ($cmd) parsing: network_address not found\n");
+               colomboCigri::add_new_cluster_event($dbh,$cluster,0,"UPDATOR_PBSNODES_PARSE",
+                           "There is an error in the oarnodes command ($cmd) parsing: network_address not found");
+               return(-1);
+             }
+           }
+           # database update
+           iolibCigri::set_cluster_node_free_weight($dbh, $cluster, $node, $maxWeight-$jobs);
+           #print "$node: $maxWeight-$jobs\n";
         }
-    }else{
-        print("[UPDATOR_ERROR] There is an error in the execution of the oarnodes command via SSH \n--> I disable all nodes of the cluster $cluster \n");
-        print("[UPDATOR_ERROR] $cmdResult{STDERR}\n");
-        # test if this is a ssh error
-        if (NetCommon::checkSshError($dbh,$cluster,$cmdResult{STDERR}) != 1){
-            colomboCigri::add_new_cluster_event($dbh,$cluster,0,"UPDATOR_PBSNODES_CMD","There is an error in the execution of the oarnodes command via SSH-->I disable all nodes of the cluster $cluster;$cmdResult{STDERR}");
-        }
+      }else{
+        print("[UPDATOR_ERROR] There is an error in the oarnodes command ($cmd) parsing\n");
+        colomboCigri::add_new_cluster_event($dbh,$cluster,0,"UPDATOR_PBSNODES_PARSE",
+                           "There is an error in the oarnodes command ($cmd) parsing");
         return(-1);
+      }
+    }else{
+      print("[UPDATOR_ERROR] There is an error in the execution of the oarnodes command ($cmd) via SSH\n");
+      print("  --> I disable all nodes of the cluster $cluster \n");
+      print("[UPDATOR_ERROR] $cmdDump{STDERR}\n");
+      # Test if this is an SSH error
+      if (NetCommon::checkSshError($dbh,$cluster,$cmdDump{STDERR}) != 1){
+         colomboCigri::add_new_cluster_event($dbh,$cluster,0,"UPDATOR_PBSNODES_CMD",
+              "There is an error in the execution of the oarnodes command via SSH"
+              ." -->I disable all nodes of the cluster $cluster;$cmdDump{STDERR}");
+      return(-1);
+      }
     }
     return(1);
 }
