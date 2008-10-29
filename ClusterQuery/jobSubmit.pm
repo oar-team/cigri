23567a8a2f273a597bef4e7b0064f06fea6a32bc
bzizou <bzizou@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2008-10-29 08:05:57 +0000

    Revert to 435. Will create a new cigri version
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/branches/stable-1.2@437 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- ClusterQuery/jobSubmit.pm
+++ ClusterQuery/jobSubmit.pm
@@ -45,9 +45,8 @@ my %submitCmd = ('PBS' => \&pbssubmit,
 #arg6 --> weight of the job
 #arg7 --> execution directory
 #arg8 --> jobId
-#arg9 --> jobName
 #return jobBatchId or -1 or -2 if something wrong happens
-sub jobSubmit($$$$$$$$$){
+sub jobSubmit($$$$$$$$){
     my $cluster = shift;
     my $blackNodes = shift;
     my $user = shift;
@@ -56,7 +55,6 @@ sub jobSubmit($$$$$$$$$){
     my $weight = shift;
     my $execDir = shift;
     my $jobId = shift;
-    my $jobName = shift;
 
     my $base = iolibCigri::connect();
     my %clusterProperties = iolibCigri::get_cluster_names_batch($base);
@@ -64,7 +62,7 @@ sub jobSubmit($$$$$$$$$){
     my %result ;
     my $retCode = -1;
     if (defined($cluster) && defined($clusterProperties{$cluster})){
-        $retCode = &{$submitCmd{$clusterProperties{$cluster}}}($base,$cluster,$blackNodes,$user,$jobFile,$walltime,$weight,$execDir,$clusterResourceUnit{$cluster},$jobId,$jobName);
+        $retCode = &{$submitCmd{$clusterProperties{$cluster}}}($base,$cluster,$blackNodes,$user,$jobFile,$walltime,$weight,$execDir,$clusterResourceUnit{$cluster},$jobId);
     }
     iolibCigri::disconnect($base);
     return($retCode);
@@ -95,7 +93,7 @@ sub endJobSubmissions($){
 #arg8 --> execDir
 #arg9 --> resrouce unit (cpu or core, or...)
 #arg10 -> jobId
-sub pbssubmit($$$$$$$$$$$){
+sub pbssubmit($$$$$$$$$$){
     my $dbh = shift;
     my $cluster = shift;
     my $blackNodes = shift;
@@ -106,7 +104,6 @@ sub pbssubmit($$$$$$$$$$$){
     my $execDir = shift;
     my $resourceUnit = shift;
     my $jobId = shift;
-    my $jobName = shift;
 
     print("PBS NOT IMPLEMENTED -- $cluster\n");
     return(-1);
@@ -122,11 +119,10 @@ sub pbssubmit($$$$$$$$$$$){
 #arg8 --> execDir
 #arg9 --> resrouce unit (cpu or core, or...)
 #arg10 -> jobId
-#arg11 -> jobName
 #return jodBatchId or
 #   -1 : for a command execution error
 #   -2 : for a jobId parse error
-sub oarsubmit($$$$$$$$$$$){
+sub oarsubmit($$$$$$$$$$){
     my $dbh = shift;
     my $cluster = shift;
     my $blackNodes = shift;
@@ -137,7 +133,6 @@ sub oarsubmit($$$$$$$$$$$){
     my $execDir = shift;
     my $resourceUnit = shift;
     my $jobId = shift;
-    my $jobName = shift;
 
     #print("$cluster --> OAR\n");
     #my $weight = iolibCigri::get_cluster_default_weight($dbh,$cluster);
@@ -183,11 +178,10 @@ sub oarsubmit($$$$$$$$$$$){
 #arg8 --> execDir
 #arg9 --> resrouce unit (cpu or core, or...)
 #arg10 -> jobId
-#arg10 -> jobName
 #return jodBatchId or
 #   -1 : for a command execution error
 #   -2 : for a jobId parse error
-sub oarsubmit2($$$$$$$$$$){
+sub oarsubmit2($$$$$$$$$){
     my $dbh = shift;
     my $cluster = shift;
     my $blackNodes = shift;
@@ -199,8 +193,6 @@ sub oarsubmit2($$$$$$$$$$){
     my $resourceUnit = shift;
     if ($resourceUnit eq '') {$resourceUnit="cpu";}
     my $jobId = shift;
-    my $jobName = shift;
-    if (!defined($jobName)) { $jobName="cigri.$jobId" } 
 
     #print("$cluster --> OAR2\n");
     #my $weight = iolibCigri::get_cluster_default_weight($dbh,$cluster);
@@ -217,7 +209,7 @@ sub oarsubmit2($$$$$$$$$$){
     }
     #print("Property String = $propertyString\n");
     #my %cmdResult = SSHcmd::submitCmd($cluster,"cd ~$user; sudo -H -u $user oarsub -l nodes=1,weight=$weight,walltime=$walltime -p \"$propertyString\" -q besteffort $jobFile");
-    my %cmdResult = SSHcmd::submitCmd($cluster,"sudo -H -u $user bash -l -c \"cd $execDir; oarsub --name=\\\"$jobName\\\" -d $execDir -l nodes=1/$resourceUnit=$weight,walltime=$walltime -p \\\"$propertyString\\\" -t besteffort $jobFile\"");
+    my %cmdResult = SSHcmd::submitCmd($cluster,"sudo -H -u $user bash -l -c \"cd $execDir; oarsub -d $execDir -l nodes=1/$resourceUnit=$weight,walltime=$walltime -p \\\"$propertyString\\\" -t besteffort $jobFile\"");
     #print(Dumper(%cmdResult));
     if ($cmdResult{STDERR} ne ""){
         # test if this is a ssh error
