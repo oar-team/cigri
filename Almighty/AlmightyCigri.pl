144c39ccfbca700c1d8d1c2f066d2b43f52407c3
bzizou <bzizou@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2009-09-10 19:33:19 +0000

    Changed the place of gridstatus in the execution order: Now that the number of running jobs is fetched from the clusters (and no more guessed from the cigri job table), we can start gridstatus just after updator. Then, data of the gridstatus table is more accurate for the scheduler.
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/trunk@550 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- Almighty/AlmightyCigri.pl
+++ Almighty/AlmightyCigri.pl
@@ -165,12 +165,12 @@ LBL:while (1){
 	$exitValue = autofix();
     next LBL if ($exitValue != 0);
     $exitValue = updator();
+	next LBL if ($exitValue != 0);
+	$exitValue = gridstatus();
     next LBL if ($exitValue != 0);
 	$exitValue =  spritz();
     next LBL if ($exitValue != 0);
     $exitValue = metascheduler();
-	next LBL if ($exitValue != 0);
-	$exitValue = gridstatus();
     next LBL if ($exitValue != 0);
     $exitValue = runner();
 	next LBL if ($exitValue != 0);
