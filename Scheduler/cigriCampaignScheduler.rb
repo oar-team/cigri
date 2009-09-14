e7d7a0397cc8c272cff4739fccadd5f1274cc8b3
bzizou <bzizou@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2009-09-14 14:56:46 +0000

    Reintegrated MAX_JOBS parameter to prevent cigri from going into too long cycles
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/trunk@554 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- Scheduler/cigriCampaignScheduler.rb
+++ Scheduler/cigriCampaignScheduler.rb
@@ -1,5 +1,21 @@
 # CiGri Campaign Scheduler definitions
 
+# Configuration loading
+if ENV['CIGRIDIR']
+  require ENV['CIGRIDIR']+'/ConfLib/conflibCigri.rb'
+else
+  require File.dirname($0)+'/../ConfLib/conflibCigri.rb'
+end
+$:.replace([get_conf("INSTALL_PATH")+"/Iolib/"] | $:)
+$:.unshift(File.dirname($0))
+
+if get_conf("MAX_JOBS")
+  $max_jobs=get_conf("MAX_JOBS").to_i
+else
+  $max_jobs=1000
+end
+
+
 ##########################################################
 # MJob Test Scheduler
 ##########################################################
@@ -21,6 +37,7 @@ end
 class DefaultScheduler
 	def self.schedule(mjob, cluster_name, free_resources)
 		nb_jobs_to_submit = free_resources * mjob.job_ratio(cluster_name)
+		nb_jobs_to_submit = $max_jobs if nb_jobs_to_submit.to_i > $max_jobs
 	        used_nodes = mjob.add_job_to_launch(cluster_name, nb_jobs_to_submit)
 		puts "[DEFAULT_SCHEDULER added toLaunch MJob: #{mjob.mjobid}; cluster; #{cluster_name}; nb jobs: #{used_nodes} " if used_nodes > 0
 		return used_nodes
