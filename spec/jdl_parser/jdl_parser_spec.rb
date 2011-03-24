f75dafc84de2fa1ad2d544fc796d738e6579d41f
gcharrie <gcharrie@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2011-03-24 16:07:50 +0000

    adding a few more rspecs
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/branches/cigri-3@658 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- spec/jdl_parser/jdl_parser_spec.rb
+++ spec/jdl_parser/jdl_parser_spec.rb
@@ -7,13 +7,15 @@ describe 'jdl_parser' do
     it 'should success with the cluster options used' do
       lambda{Cigri::JDLParser.parse('{"name":"n", "nb_jobs":0,"clusters":{"c":{"exec_file":"e"}}}')}.should_not raise_error
     end
-    
     it 'should success with the global options used' do
       lambda{Cigri::JDLParser.parse('{"name":"n", "nb_jobs":0,"exec_file":"e","clusters":{"c":{}}}')}.should_not raise_error
     end
+    it 'should success with the global options used' do
+      lambda{Cigri::JDLParser.parse('{"name":"n", "jobs_type":"desktop_computing","exec_file":"e","clusters":{"c":{}}}')}.should_not raise_error
+    end
   end # successes with minimal campaigns
   
-  describe 'missing parameters' do
+  describe 'missing fields' do
     it 'should fail if name missing' do
       lambda{Cigri::JDLParser.parse('{"nb_jobs":0,"clusters":{"c":{"exec_file":"e"}}}')}.should raise_error Cigri::Exception
     end
@@ -26,5 +28,21 @@ describe 'jdl_parser' do
     it 'should fail if no cluster defined' do
       lambda{Cigri::JDLParser.parse('{"name":"n", "nb_jobs":0, "clusters":{}}')}.should raise_error Cigri::Exception
     end
+  end # missing parameters
+  
+  describe 'parameters file' do
+    it 'should success if param_file given' do
+      lambda{Cigri::JDLParser.parse('{"name":"n","param_file":"p","clusters":{"c":{"exec_file":"e"}}}')}.should_not raise_error
+    end
+    it 'should success if nb_jobs given' do
+      lambda{Cigri::JDLParser.parse('{"name":"n","nb_jobs":0,"clusters":{"c":{"exec_file":"e"}}}')}.should_not raise_error
+    end
+    it 'should success if jobs_type is desktop_computing given' do
+      lambda{Cigri::JDLParser.parse('{"name":"n","jobs_type":"desktop_computing","clusters":{"c":{"exec_file":"e"}}}')}.should_not raise_error
+    end
+    it 'should fail if no param_file or nb_jobs or jobs_type=desktop_computing given' do
+      lambda{Cigri::JDLParser.parse('{"name":"n","clusters":{"c":{"exec_file":"e"}}}')}.should raise_error Cigri::Exception
+    end
   end
+  
 end # jdl_parser
