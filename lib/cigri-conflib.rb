0b2c9be9ad5354224320e33db31ff1bb9ae784a6
bzizou <bzizou@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2011-03-25 15:01:15 +0000

    Conflib from cigri 1
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/branches/cigri-3@665 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

---
+++ lib/cigri-conflib.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby -w
+# Cigri configuration library
+
+if ENV['CIGRICONFDIR']
+then
+  config_file="#{ENV['CIGRICONFDIR']}/cigri.conf"
+elsif ENV['CIGRIDIR'] && File.exists?("#{ENV['CIGRIDIR']}/cigri.conf")
+  config_file="#{ENV['CIGRIDIR']}/cigri.conf"
+else
+  config_file="/etc/cigri.conf"
+end
+
+
+begin
+  file=File.new(config_file,"r")
+rescue
+  puts "Unable to open config file #{config_file}!"
+end
+
+$conf={}
+file.each do |line|
+  a=line.scan(/^\s*([^#=\s]+)\s*=\s*"([^#]*)"/)
+  key,val=a[0]
+  $conf[key]=val if key
+end
+
+def get_conf(key)
+  $conf[key]
+end
