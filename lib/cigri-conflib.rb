892b71c3f5a4509e9b4ec71ae0601084e4d2b39f
bzizou <bzizou@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2011-03-29 11:34:50 +0000

    Better conflib
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/branches/cigri-3@671 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- lib/cigri-conflib.rb
+++ lib/cigri-conflib.rb
@@ -1,29 +1,57 @@
 #!/usr/bin/ruby -w
+#
 # Cigri configuration library
+#
+# Usage example:
+#
+#$LOAD_PATH << './lib'
+#require 'cigri-conflib'
+#config=CigriConflib::Conf.new
+#puts config.get("INSTALL_PATH")
+#
+#
 
-if ENV['CIGRICONFDIR']
-then
-  config_file="#{ENV['CIGRICONFDIR']}/cigri.conf"
-elsif ENV['CIGRIDIR'] && File.exists?("#{ENV['CIGRIDIR']}/cigri.conf")
-  config_file="#{ENV['CIGRIDIR']}/cigri.conf"
-else
-  config_file="/etc/cigri.conf"
-end
+module CigriConflib
 
-
-begin
-  file=File.new(config_file,"r")
-rescue
-  puts "Unable to open config file #{config_file}!"
-end
-
-$conf={}
-file.each do |line|
-  a=line.scan(/^\s*([^#=\s]+)\s*=\s*"([^#]*)"/)
-  key,val=a[0]
-  $conf[key]=val if key
-end
-
-def get_conf(key)
-  $conf[key]
+  if ENV['CIGRICONFDIR'] && File.exists?("#{ENV['CIGRICONFDIR']}/cigri.conf")
+  then
+    CONFIG_FILE="#{ENV['CIGRICONFDIR']}/cigri.conf"
+  elsif ENV['CIGRIDIR'] && File.exists?("#{ENV['CIGRIDIR']}/cigri.conf")
+    CONFIG_FILE="#{ENV['CIGRIDIR']}/cigri.conf"
+  elsif File.exists?("./cigri.conf")
+    CONFIG_FILE="./cigri.conf"
+  else
+    CONFIG_FILE="/etc/cigri.conf"
+  end
+  
+  class Conf
+    attr_reader :conf
+    attr_accessor :config_file
+  
+    def initialize(config_file = CONFIG_FILE)
+      begin
+        @file=File.new(config_file,"r")
+        self.scan
+      rescue
+        warn "Conf: unable to open config file #{config_file}!"
+      end
+    end
+  
+    def scan
+      @conf={}
+      @file.each do |line|
+        a=line.scan(/^\s*([^#=\s]+)\s*=\s*"([^#]*)"/)
+        key,val=a[0]
+        @conf[key]=val if key
+      end
+    end  
+  
+    def get(key)
+      if @conf.has_key?(key)
+        return @conf[key]
+      else
+        warn "Conf: no key #{key}"
+      end
+    end
+  end 
 end
