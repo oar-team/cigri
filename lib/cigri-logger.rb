6cb5404c217b1c3c4660acf1f9cb23bb9da2573a
gcharrie <gcharrie@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2011-04-04 13:39:05 +0000

    reverting previous mistake commit
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/branches/cigri-3@693 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- lib/cigri-logger.rb
+++ lib/cigri-logger.rb
@@ -2,49 +2,29 @@ require 'logger'
 require 'cigri-conflib'
 
 module Cigri
-
-  logger = nil
-  
-  def logger
-    return @logger if @logger
-    
-    config = Cigri::Conf.new
-    level = config.get('LOG_LEVEL') if config.exists?('LOG_LEVEL')
-    logdev = STDOUT if logdev.eql? 'STDOUT'
-    logdev = STDERR if logdev.eql? 'STDERR'
-    logger = Logger.new(logdev)
-    logger.progname = progname
-    logger.level = Logger.const_get(level)
-    logger.formatter = proc { |severity, datetime, progname, msg|
-      date = datetime.strftime('%Y-%m-%d %H:%M:%S.') << "%d" % datetime.usec
-      "[%5s] [#{date}] [#{progname}] #{msg}\n" % severity
-    }
-    @logger = logger
+  ##
+  # Specific Logger for Cigri. 
+  # Changes the ruby logger output and needs a progname
+  ##
+  class Logger < Logger
+    ##
+    # Create a new Logger: logger = Logger.new('module', STDOUT [, shift_age, shift_size])
+    # Params:
+    #   - progname: Name of the module you want to log
+    #   - logdev: location of the file to save the log (can be STDOUT or STDERR). Default = STDOUT
+    ##
+    def initialize(progname, logdev = 'STDOUT')
+      config = Cigri::Conf.new
+      level = config.get('LOG_LEVEL') if config.exists?('LOG_LEVEL')
+      logdev = STDOUT if logdev.eql? 'STDOUT'
+      logdev = STDERR if logdev.eql? 'STDERR'
+      super(logdev)
+      self.progname = progname
+      self.level = Logger.const_get(level)
+      self.formatter = proc { |severity, datetime, progname, msg|
+        date = datetime.strftime('%Y-%m-%d %H:%M:%S.') << "%d" % datetime.usec
+        "[%5s] [#{date}] [#{progname}] #{msg}\n" % severity
+      }
+    end
   end
-  
-#  ##
-#  # Specific Logger for Cigri. 
-#  # Changes the ruby logger output and needs a progname
-#  ##
-#  class Logger < Logger
-#    ##
-#    # Create a new Logger: logger = Logger.new('module', STDOUT [, shift_age, shift_size])
-#    # Params:
-#    #   - progname: Name of the module you want to log
-#    #   - logdev: location of the file to save the log (can be STDOUT or STDERR). Default = STDOUT
-#    ##
-#    def initialize(progname, logdev = 'STDOUT')
-#      config = Cigri::Conf.new
-#      level = config.get('LOG_LEVEL') if config.exists?('LOG_LEVEL')
-#      logdev = STDOUT if logdev.eql? 'STDOUT'
-#      logdev = STDERR if logdev.eql? 'STDERR'
-#      super(logdev)
-#      self.progname = progname
-#      self.level = Logger.const_get(level)
-#      self.formatter = proc { |severity, datetime, progname, msg|
-#        date = datetime.strftime('%Y-%m-%d %H:%M:%S.') << "%d" % datetime.usec
-#        "[%5s] [#{date}] [#{progname}] #{msg}\n" % severity
-#      }
-#    end
-#  end
 end
