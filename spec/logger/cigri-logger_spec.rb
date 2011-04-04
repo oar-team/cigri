2267c13b643380d6f9591e0ce08aff93919fbe5e
gcharrie <gcharrie@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2011-04-04 14:28:55 +0000

    updating rspec to stay synched with logger
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/branches/cigri-3@697 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- spec/logger/cigri-logger_spec.rb
+++ spec/logger/cigri-logger_spec.rb
@@ -16,20 +16,20 @@ describe 'cigri-logger' do
     it 'should fail when no arg given' do
       lambda{Cigri::Logger.new()}.should raise_error Exception
     end
-    it 'should fail when 1 arg given' do
-      lambda{Cigri::Logger.new('spec')}.should raise_error Exception
-    end
-    it 'should fail when 2 args given and second arg not stream or writable file' do
+    it 'should fail when 2 args given and second arg not a writable file' do
       lambda{Cigri::Logger.new('spec', '/detzkfuidhb/hfgsdh')}.should raise_error Exception
+    end
+    it 'should fail when more than 2 args given' do
+      lambda{Cigri::Logger.new('spec', '/dev/null', 10)}.should raise_error Exception
     end
   end # initialize failures
   
   describe 'initialize successes' do    
+    it 'should success when 1 arg given' do
+      lambda{Cigri::Logger.new('spec')}.should_not raise_error Exception
+    end
     it 'should success when 2 args given' do
       lambda{Cigri::Logger.new('spec', '/dev/null')}.should_not raise_error Exception
-    end
-    it 'should success when many args given' do
-      lambda{Cigri::Logger.new('spec', '/dev/null', Cigri::Logger::FATAL, 10, 10)}.should_not raise_error Exception
     end
   end # initialize successes
   
@@ -39,51 +39,34 @@ describe 'cigri-logger' do
     end
     
     it 'should print only fatal' do
-      logger = Cigri::Logger.new('spec', file, Cigri::Logger::FATAL)
+      logger = Cigri::Logger.new('spec', file)
+      logger.level = Cigri::Logger::FATAL
       log(logger)
       File.readlines(file).length.should == 2
     end
     it 'should print errors' do
-      logger = Cigri::Logger.new('spec', file, Cigri::Logger::ERROR)
+      logger = Cigri::Logger.new('spec', file)
+      logger.level = Cigri::Logger::ERROR
       log(logger)
       File.readlines(file).length.should == 3
     end
     it 'should print warnings' do
-      logger = Cigri::Logger.new('spec', file, Cigri::Logger::WARN)
+      logger = Cigri::Logger.new('spec', file)
+      logger.level = Cigri::Logger::WARN
       log(logger)
       File.readlines(file).length.should == 4
     end
     it 'should print info' do
-      logger = Cigri::Logger.new('spec', file, Cigri::Logger::INFO)
+      logger = Cigri::Logger.new('spec', file)
+      logger.level = Cigri::Logger::INFO
       log(logger)
       File.readlines(file).length.should == 5
     end
     it 'should print everything' do
-      logger = Cigri::Logger.new('spec', file, Cigri::Logger::DEBUG)
+      logger = Cigri::Logger.new('spec', file)
+      logger.level = Cigri::Logger::DEBUG
       log(logger)
       File.readlines(file).length.should == 6
     end
   end # different logging levels
-  
-  describe 'shift options' do
-    it 'should generate 2 logfiles' do
-      logger = Cigri::Logger.new('spec', file, Cigri::Logger::DEBUG, 2, 200)
-      10.times{log(logger)}
-      files = Dir.glob("#{file}*")
-      files.length.should == 2
-      files.each {|f| File.delete(f)}
-    end
-    it 'should be smaller than the given size' do
-      logger = Cigri::Logger.new('spec', file, Cigri::Logger::DEBUG, 1, 256)
-      log(logger) #log procuces a file of size 417
-      File.size(file).should <= 256
-      File.delete(file)
-    end
-    it 'should be smaller than the given size' do
-      logger = Cigri::Logger.new('spec', file, Cigri::Logger::DEBUG, 1, 200)
-      log(logger) #log procuces 6 lines
-      File.readlines(file).length.should == 3
-      File.delete(file)
-    end
-  end # shift options
 end # cigri-logger
