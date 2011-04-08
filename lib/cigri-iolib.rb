d49895d3f0b586c76b69e919d2ad1b241fa2f8b9
gcharrie <gcharrie@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2011-04-08 14:30:53 +0000

    update dabatase schema + submit campaign now adds camapigns_properties
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/branches/cigri-3@704 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- lib/cigri-iolib.rb
+++ lib/cigri-iolib.rb
@@ -1,4 +1,5 @@
 require 'cigri-conflib'
+require 'cigri-exception'
 require 'cigri-logger'
 require 'cigri-utils'
 require 'json'
@@ -13,20 +14,50 @@ LOGGER = Cigri::Logger.new('IOLIB', CONF.get('LOG_FILE'))
 # == Parameters
 # - dbh: database handle
 # - json: expended json corresponding to JDL
-# - user: username in the daatabase
+# - user: username in the database
 # 
 def cigri_submit(dbh, json, user)
+  LOGGER.debug('Saving campaign into database')
   dbh['AutoCommit'] = false
   begin
-    LOGGER.debug('Saving campaign into database')
+    #INSERT INTO campaigns
     query = 'INSERT into campaigns 
              (grid_user, state, type, name, submission_time, jdl)
              VALUES (?, \'in_treatment\', ?, ?, NOW(),  ?)'
     dbh.do(query, user, json['jobs_type'], json['name'], json.to_json)
+    
     campaign_id = last_inserted_id(dbh, 'campaigns_id_seq')
+    
+    #add campaigns properties
+    at_least_one_cluster = false
+    sth = dbh.prepare("SELECT id FROM clusters where name = ?")
+    json['clusters'].each do |cluster|
+      sth.execute(cluster[0])
+      cluster_id = sth.first
+      if cluster_id
+        cluster_id = cluster_id[0]
+        at_least_one_cluster = true
+        query = 'INSERT INTO campaign_properties 
+                 (name, value, cluster_id, campaign_id)
+                 VALUES (?, ?, ?, ?)'
+        dbh.do(query, '', '', cluster_id, campaign_id)
+      else
+        LOGGER.warn("Cluster #{cluster[0]} unknown, campaign_property not added for this cluster")
+      end
+    end
+    raise Cigri::Exception, "No clusters usable for the campaign" unless at_least_one_cluster
+    sth.finish()
+    
+    #create bag_of_tasks
+    unless json['jobs_type'].eql?('desktop_computing')
+      
+    else
+      raise Cigri::Exception, 'Desktop_computing campaigns are not yet sopported'
+    end
+    
     dbh.commit()
   rescue Exception => e
-    LOGGER.error('Error ruing campaign submission: ' + e.message)
+    LOGGER.error('Error running campaign submission: ' + e.inspect)
     dbh.rollback()
     raise e
   ensure
