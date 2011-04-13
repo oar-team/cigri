3b2aa8d19251398cd618dc02e5ef74fb4e8aa154
gcharrie <gcharrie@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2011-04-13 09:56:55 +0000

    forgotten file: fill some clusters
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/branches/cigri-3@713 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

---
+++ spec/init_fill.sql
@@ -0,0 +1,4 @@
+INSERT INTO clusters (name, api_url, api_username, api_password, batch) VALUES 
+  ('my.cluster.fr', 'http://www.my.cluster.fr/jobs', 'Username', 'Password', 'g5k');
+INSERT INTO clusters (name, api_url, api_username, api_password, batch) VALUES 
+  ('my.last_cluster.fr', 'http://www.my.last.cluster.fr/jobs', 'Username', 'Password', 'g5k');
