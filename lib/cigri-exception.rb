ff401810f2170957ec5e2cb7ca4072984c3f990d
gcharrie <gcharrie@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2011-05-04 09:06:56 +0000

    Exceptions write in Logger if it is defined
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/branches/cigri-3@739 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- lib/cigri-exception.rb
+++ lib/cigri-exception.rb
@@ -1,7 +1,14 @@
 module Cigri
   ##
-  # Cigri Exceptions are just normal exceptions
+  # Cigri Exceptions are just normal exceptions that are logger if 
+  # LOGGER exists
   ##
   class Exception < Exception
+    def initialize(msg)
+      if defined? LOGGER and LOGGER.class == Cigri::Logger
+        LOGGER.error('Exception raised: ' + msg)
+      end
+      super
+    end
   end
 end
