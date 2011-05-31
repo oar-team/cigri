0298824b2cd0fbb83df72841e37dfea68e2ef27e
gcharrie <gcharrie@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2011-05-31 08:08:57 +0000

    griddel can take several campaigns as parameter
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/branches/cigri-3@785 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- bin/griddel.rb
+++ bin/griddel.rb
@@ -4,18 +4,18 @@ $LOAD_PATH.unshift("#{ENV["CIGRIDIR"]}/lib")
 
 require 'cigri'
 
-abort("Usage: #{File.basename(__FILE__)} <CAMPAIGN_ID>") unless ARGV.length == 1
-
-id = ARGV[0]
+abort("Usage: #{File.basename(__FILE__)} <CAMPAIGN_ID> [CAMPAIGN_IDS...]") unless ARGV.length >= 1
 
 db_connect() do |dbh|
-  res = delete_campaign(dbh, ENV["SUDO_USER"], id)
-  if res == nil
-    puts "Campaign #{id} does not exist"
-  elsif res
-    puts "Campaign #{id} deleted"
-  else
-    puts "Campaign #{id} does not belong to you"
+  for id in ARGV do
+    res = delete_campaign(dbh, ENV["SUDO_USER"], id)
+    if res == nil
+      puts "Campaign #{id} does not exist"
+    elsif res
+      puts "Campaign #{id} deleted"
+    else
+      puts "Campaign #{id} does not belong to you"
+    end
   end
 end
 
