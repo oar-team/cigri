292de405a47f09e53515aa69883441f10cb799d7
bzizou <bzizou@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2011-06-22 07:38:23 +0000

    Added colors to logger in console mode
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/branches/cigri-3@813 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- lib/cigri-logger.rb
+++ lib/cigri-logger.rb
@@ -16,15 +16,46 @@ module Cigri
     def initialize(progname, logdev = 'STDOUT')
       config = Cigri.conf
       level = config.get('LOG_LEVEL') if config.exists?('LOG_LEVEL')
+      @logdev_name=logdev
       logdev = STDOUT if logdev.eql? 'STDOUT'
       logdev = STDERR if logdev.eql? 'STDERR'
       super(logdev)
       self.progname = progname
       self.level = Logger.const_get(level)
-      self.formatter = proc { |severity, datetime, progname, msg|
-        date = datetime.strftime('%Y-%m-%d %H:%M:%S.') << "%d" % datetime.usec
-        "[%5s] [#{date}] [#{progname}] #{msg}\n" % severity
-      }
     end
+
+    private
+
+    # Formatting mods
+
+    def format_message(severity, datetime, progname, msg)
+      date = datetime.strftime('%Y-%m-%d %H:%M:%S.') << "%d" % datetime.usec
+      # Less info and colored on the console
+      if @logdev_name == 'STDOUT' or @logdev_name == 'STDERR'
+        date = datetime.strftime('%Y-%m-%d %H:%M:%S')
+        log_str = "[#{date}][#{severity[0...1]}][#{progname}] #{color_msg(msg, severity, progname)}\n"
+      else
+        date = datetime.strftime('%Y-%m-%d %H:%M:%S.') << "%d" % datetime.usec
+        log_str = "[#{date}][#{severity[0...1]}][#{$$}][#{progname}] #{msg}\n"
+      end
+    end
+
+    def color_msg msg, severity, progname 
+      # Background color depending on severity
+      back_color = severity[0...1] == 'E' ? '41' : '40' # Red for errors, black otherwize
+      # Text color depending on the progname
+      case 
+        when progname == "ALMIGHTY"
+          fore_color = 32
+        when progname.match(/RUNNER/)
+          fore_color = 34
+        when progname.match(/SCHEDULER/)
+          fore_color = 36
+        else
+          fore_color = 30
+      end
+      "\033[1;#{back_color}m\033[#{fore_color}m#{msg}\033[0m"
+    end
+
   end
 end
