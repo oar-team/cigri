40c523c8ff1ff5b08e6e54594fec2a49aff690f8
gcharrie <gcharrie@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2011-08-18 09:03:08 +0000

    adding 'gridstat -u USERNAME' option
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/branches/cigri-3@835 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- bin/gridstat.rb
+++ bin/gridstat.rb
@@ -3,12 +3,39 @@
 $LOAD_PATH.unshift("#{ENV["CIGRIDIR"]}/lib")
 
 require 'cigri'
-require 'net/http'
 require 'json'
-require 'pp'
+require 'net/http'
+require 'optparse'
 
-campaigns = JSON.parse(Net::HTTP.get('localhost', '/campaigns', 9292))
+username = nil
+optparse = OptionParser.new do |opts|
+  opts.banner = 'Usage: gridstat [options] ...'
+  opts.version = "v#{Cigri::VERSION}"
+  
+  opts.on( '-u', '--username USERNAME', String, 'Only print campaigns from USERNAME' ) do |u|
+    username = u
+  end
+  
+  opts.on( '-h', '--help', 'Display this screen' ) do
+    puts opts
+    exit
+  end
+end
 
-campaigns['items'].each do |campaign|
+begin
+  optparse.parse!(ARGV)
+rescue OptionParser::ParseError => e
+  $stderr.puts e
+  $stderr.puts "\n" + optparse.to_s
+  exit 1
+end
+
+campaigns = JSON.parse(Net::HTTP.get('localhost', '/campaigns', 9292))['items']
+
+if username
+  campaigns.reject!{|h| h['user'].nil? || h['user'] != username}
+end
+
+campaigns.each do |campaign|
   pp campaign
 end
