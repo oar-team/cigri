f68b310952704104be922cb404106e77b70a1e48
gcharrie <gcharrie@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2011-08-26 09:50:18 +0000

    griddel now uses the API
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/branches/cigri-3@856 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- bin/griddel.rb
+++ bin/griddel.rb
@@ -1,21 +1,54 @@
 #!/usr/bin/ruby -w
 
-$LOAD_PATH.unshift("#{ENV["CIGRIDIR"]}/lib")
+$LOAD_PATH.unshift(File.join(File.dirname(File.expand_path(__FILE__)), '..', 'lib'))
 
-require 'cigri'
+require 'cigri-conflib'
+require 'json'
+require 'net/http'
+require 'optparse'
+require 'version.rb'
+require 'pp'
 
-abort("Usage: #{File.basename(__FILE__)} <CAMPAIGN_ID> [CAMPAIGN_IDS...]") unless ARGV.length >= 1
+verbose = false
+optparse = OptionParser.new do |opts|
+  opts.banner = "Usage:  #{File.basename(__FILE__)} <CAMPAIGN_ID> [CAMPAIGN_IDS...] [options]"
+  
+  opts.on('-v', '--verbose', 'Be verbose') do
+    verbose = true
+  end
+  
+  opts.on( '-V', '--version', 'Display Cigri version' ) do
+    puts "#{File.basename(__FILE__)} v#{Cigri::VERSION}"
+    exit
+  end
+  
+  opts.on( '-h', '--help', 'Display this screen' ) do
+    puts opts
+    exit
+  end
+end
 
-db_connect() do |dbh|
-  for id in ARGV do
-    res = delete_campaign(dbh, ENV["SUDO_USER"], id)
-    if res == nil
-      puts "Campaign #{id} does not exist"
-    elsif res
-      puts "Campaign #{id} deleted"
-    else
-      puts "Campaign #{id} does not belong to you"
-    end
+begin
+  optparse.parse!(ARGV)
+rescue OptionParser::ParseError => e
+  $stderr.puts e
+  $stderr.puts "\n" + optparse.to_s
+  exit 1
+end
+
+abort("Missing CAMPAIGN_ID\n" + optparse.to_s) unless ARGV.length > 0
+
+conf = Cigri::Conf.new('/etc/cigri-api.conf')
+http = Net::HTTP.new(conf.get('API_HOST'), conf.get('API_PORT'))
+
+ARGV.each do |campaign_id|
+  request = Net::HTTP::Delete.new("/campaigns/#{campaign_id}")
+  response = http.request(request)
+  parsed_response = JSON.parse(response.body)
+  if response.code != "202"
+    STDERR.puts("Failed to cancel campaign #{campaign_id}: #{parsed_response['message']}.")
+  else
+    puts "Campaign #{campaign_id} cancelled." if verbose
   end
 end
 
