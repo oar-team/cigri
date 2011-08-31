cbc5a7691b93ada3136f8758ecefa84c09e4def7
gcharrie <gcharrie@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2011-08-31 12:16:56 +0000

    first rspec on APi
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/branches/cigri-3@864 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

---
+++ spec/api/cigri-api_spec.rb
@@ -0,0 +1,56 @@
+require File.join(File.expand_path(File.dirname(__FILE__)), '..', '..', 'api', 'cigri-api.rb')
+require 'cigri'
+require 'spec_helper'
+require 'rack/test'
+require 'sinatra'
+
+set :environment, :test
+
+def app
+  API
+end
+
+describe 'API' do
+  include Rack::Test::Methods
+    
+  ['/', '/clusters', '/campaigns'].each do |url|
+    it "should success to get the url '#{url}'" do
+      get url
+      last_response.should be_ok
+      response = JSON.parse last_response.body
+      links = response['links']
+      links.should_not be nil
+    end
+  end
+  
+  describe 'Interactive commands' do
+    before(:all) do
+      post '/campaigns', '{"name":"test_api", "nb_jobs":10,"clusters":{"fukushima":{"exec_file":"e"}}}'
+      response = JSON.parse last_response.body
+      @test_id = response['id']
+    end
+    
+    it 'should get info relative to the posted campaign' do
+      get "/campaigns/#{@test_id}"
+      last_response.should be_ok
+      response = JSON.parse last_response.body
+      links = response['links']
+      links.should_not be nil
+    end
+    
+    xit 'should get info relative to the jobs posted campaign' do
+      get "/campaigns/#{@test_id}/jobs"
+      last_response.should be_ok
+      response = JSON.parse last_response.body
+      links = response['links']
+      links.should_not be nil
+    end
+    
+    after(:all) do
+      db_connect do |dbh|
+        delete_campaign(dbh, 'API', @test_id)
+      end
+    end
+  end
+end
+
