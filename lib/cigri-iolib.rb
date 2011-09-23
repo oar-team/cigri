cd552fcf6b141d2109b273527b46125a19dddf4e
gcharrie <gcharrie@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2011-09-23 11:53:57 +0000

    use stringIO to prepare insertion of many tasks in bag_of tasks (huge speed improvement for large datasets)
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/branches/cigri-3@871 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- lib/cigri-iolib.rb
+++ lib/cigri-iolib.rb
@@ -127,12 +127,20 @@ def cigri_submit(dbh, json, user)
     
     #create bag_of_tasks
     unless json['jobs_type'].eql?('desktop_computing')
-      query = 'INSERT INTO bag_of_tasks 
-               (name, param, campaign_id)
-               VALUES (?, ?, ?)'
+      query = StringIO.new
+      query << 'INSERT INTO bag_of_tasks 
+                (name, param, campaign_id)
+                VALUES '
+      first = true
       json['params'].each do |param|
-        dbh.do(query, param.split.first, param, campaign_id)
+        if first
+          first = false
+        else
+          query << ','
+        end
+        query << "('#{param.split.first}', '#{param}', '#{campaign_id}')"
       end
+      dbh.do(query.string)
     else
       raise Cigri::Exception, 'Desktop_computing campaigns are not yet sopported'
     end
