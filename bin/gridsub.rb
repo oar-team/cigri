58dd0c9e7cf31d79044d047223ece34c5c152dfb
gcharrie <gcharrie@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2011-11-21 16:51:39 +0000

    gridsub now supports adding jobs to a campaign
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/branches/cigri-3@889 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- bin/gridsub.rb
+++ bin/gridsub.rb
@@ -7,13 +7,18 @@ require 'json'
 require 'net/http'
 require 'optparse'
 require 'version.rb'
-require 'pp'
 
 jsons = []
+campaign_id = nil
+jobs = []
 details = false
 optparse = OptionParser.new do |opts|
   opts.banner = "Usage:  #{File.basename(__FILE__)} <-j JDL | -f JDL_FILE> [options]"
   
+  opts.on( '-c', '--campaign CAMPAIGN_ID', 'Add jobs in an existing campaign' ) do |id|
+    campaign_id = id
+  end
+
   opts.on( '-d', '--details', 'Prints all the JSON of the campaign instead of just the ID' ) do
     details = true
   end
@@ -21,9 +26,17 @@ optparse = OptionParser.new do |opts|
   opts.on( '-f', '--file JDL_FILE', 'JDL File containing the JSON' ) do |file|
     jsons << File.read(file)
   end
+
+  opts.on( '-F', '--jobfile JDL_FILE', 'JDL File containing an array of parameters' ) do |file|
+    jobs << File.read(file)
+  end
   
   opts.on( '-j', '--json JSON', 'JSON String' ) do |json|
     jsons << json
+  end
+
+  opts.on( '-J', '--jsonjob JSON', 'JSON String containing an array of parameters' ) do |job|
+    jobs << job
   end
   
   opts.on( '--version', 'Display Cigri version' ) do
@@ -40,32 +53,50 @@ end
 begin
   optparse.parse!(ARGV)
 rescue OptionParser::ParseError => e
-  $stderr.puts e
-  $stderr.puts "\n" + optparse.to_s
+  STDERR.puts e
+  STDERR.puts "\n" + optparse.to_s
   exit 1
 end
 
-def submit_campaign(jdl)
+def submit_campaign(jdl, campaign_id=nil)
   conf = Cigri::Conf.new('/etc/cigri-api.conf')
   http = Net::HTTP.new(conf.get('API_HOST'), conf.get('API_PORT'))
   http.read_timeout = conf.get('API_TIMEOUT') if conf.exists?('API_TIMEOUT')
   
-  http.post("/campaigns", jdl, 'Content-Type' => 'application/json') 
+  url = '/campaigns'
+  url += "/#{campaign_id}/jobs" if campaign_id
+
+  http.post(url, jdl, 'Content-Type' => 'application/json', 'X_CIGRI_USER' => ENV['USER'])
 end
 
-STDERR.puts "Please provide at least one JSON or JDL_FILE" if jsons.length == 0
+def print_response(response, details)
+  
+end
+
+if jsons.length == 0 and jobs.length == 0
+  STDERR.puts "Please provide at least one JSON or JDL_FILE, or an array of parameters" 
+  STDERR.puts optparse.to_s
+end
+STDERR.puts "Please provide a campaign ID to add jobs in it" if jobs.length > 0 and campaign_id.nil?
 
 begin
   jsons.each do |json|
     response = submit_campaign(json)
-    if response.code != "201"
-      STDERR.puts("Error submitting campaign: #{response.body}")
+    if response.code == "201"
+        puts "Campaign successfully submitted"
+        puts response.body if details
+        puts "CAMPAIGN_ID=#{JSON.parse(response.body)['id']}"
     else
-      if details
-        puts response.body
-      else
-        puts JSON.parse(response.body)['id']
-      end
+      STDERR.puts("Error submitting campaign: #{response.body}")
+    end
+  end
+  jobs.each do |job|
+    response = submit_campaign(job, campaign_id)
+    if response.code == "201"
+      puts "Jobs successfully added to campaign #{campaign_id}"
+      puts response.body if details
+    else
+      STDERR.puts("Error submitting campaign: #{response.body}")
     end
   end
 rescue Errno::ECONNREFUSED => e
