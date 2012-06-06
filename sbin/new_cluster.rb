dc119ea7b1c2ea558b2f35a561104f9e1e48811f
bzizou <bzizou@a0e46a8e-710f-0410-a56b-8ab1cdea0c2d>
2012-06-06 14:17:06 +0000

    Added api_auth_method support into new_cluster.rb cmd and fixed kameleon step
    git-svn-id: https://scm.gforge.inria.fr/svn/cigri/branches/cigri-3@980 a0e46a8e-710f-0410-a56b-8ab1cdea0c2d

--- sbin/new_cluster.rb
+++ sbin/new_cluster.rb
@@ -4,12 +4,12 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'cigri-clusterlib'
 
-abort("Usage: #{File.basename(__FILE__)} <name> <api_url> <api_username> <api_password> <ssh_host> <batch> <resource_unit> <power> <properties>") unless ARGV.length == 9
+abort("Usage: #{File.basename(__FILE__)} <name> <api_url> <api_auth_type(cert|password)> <api_username> <api_password> <ssh_host> <batch> <resource_unit> <power> <properties>") unless ARGV.length == 10
 
 # Check the batch type
 available_batch_types = Cigri::Cluster.available_types
-unless available_batch_types.include?(ARGV[5])
-  raise "\"#{ARGV[5]}\" is not a valid batch system type. Valid types are: #{available_batch_types.join(', ')}"
+unless available_batch_types.include?(ARGV[6])
+  raise "\"#{ARGV[6]}\" is not a valid batch system type. Valid types are: #{available_batch_types.join(', ')}"
 end
 
 db_connect() do |dbh|
